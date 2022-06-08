3d5edb467d25e48b34ef4f64404f8ec4f6b2401c
ahdabelle <ahdasy21@gmail.com>
2022-06-08 18:27:28 +0200

    Update Usine.java

--- src/main/java/classCode/emplacement/Usine.java
+++ src/main/java/classCode/emplacement/Usine.java
@@ -1,11 +1,26 @@
 package classCode.emplacement;
 
+import java.util.ArrayList;
+
 public class Usine extends Terrain{
-    public Usine(float width, float height, float x, float y) {
+
+    private ArrayList<Ligne> tabLigne;
+    private Stock stock;
+
+    public Usine(float width, float height, float x, float y, ArrayList<Ligne> tabLigne, Stock stock) {
         super(width, height, x, y);
+        this.tabLigne = (ArrayList<Ligne>)tabLigne.clone();
+        this.stock = stock;
     }
+
     // tableau de ligne
     // stockage
     //AchaterMachine
+
+    
+    public boolean acheterMachine(int a){
+        
+        return false;
+    }
 
 }
