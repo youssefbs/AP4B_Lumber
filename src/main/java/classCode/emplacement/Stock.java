5e2f9dc10fb7dfde47b128d52b1ceb96d80052d9
ahdabelle <ahdasy21@gmail.com>
2022-06-08 17:59:42 +0200

    Update Stock.java

--- src/main/java/classCode/emplacement/Stock.java
+++ src/main/java/classCode/emplacement/Stock.java
@@ -1,4 +1,66 @@
 package classCode.emplacement;
 
-public class Stock {
+import java.util.ArrayList;
+import classCode.Joueur;
+
+public class Stock extends Terrain {
+
+    private int capacite;
+    private int stockUtiliser;
+    private int niveau;
+    private ArrayList<Float> prix;
+    private float xStock;
+    private float yStock;
+    private float widthStock;
+    private float heightStock;
+
+
+    public Stock(float width, float height, float x, float y, int capacite, int stockUtiliser, int niveau) {
+        super(width, height, x, y);
+        this.capacite = capacite;
+        this.stockUtiliser = stockUtiliser;
+        this.niveau = niveau;
+        this.prix = (ArrayList<Float>)prix.clone();
+    }
+
+    public boolean augmenterNiveau(Joueur j){
+        if(j.reduireArgent(this.prix.get(niveau)))
+            return true;
+        return false;
+    }
+
+    public boolean ajouterBois(int nb){
+        if(capacite - stockUtiliser > nb){
+                stockUtiliser= stockUtiliser + nb;
+                return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Float> getPrix(){
+        return prix;
+    }
+
+    public int getStockUtiliser(){
+        return stockUtiliser;
+    }
+
+    public int getCapacite(){
+        return capacite;
+    }
+
+    public int getNiveau(){
+        return niveau;
+    }
+
+    public float getWidthStock(){
+        return widthStock;
+    }
+
+    public float getHeightStock(){
+        return heightStock;
+    }
+
+
 }
+
